a6f6188957b6f851422ab41e0480bfbfc33bb369
GabrieleLonghitano90 <gabrielelonghitano90@gmail.com>
2024-04-06 19:35:01 +0200

    esercizi prima e seconda lezione javascript

---
+++ Esercizi 05-04-2024/main.js
@@ -0,0 +1,36 @@
+// PRIMO ESERCIZIO
+
+// let birthday = "12/07/1990" 
+
+// if (birthday === "14/07/1990") 
+//  { console.log( '🎁' ) }
+// else { console.log("oggi non è il tuo compleanno") }
+
+
+//----------------FINE PRIMO ESERCIZIO
+
+//-------------- INIZIO SECONDO ESERCIZIO
+
+
+// let azionidioggi = ["Mi sveglio","Mi alzo", "mi lavo la faccia",
+//                         "faccio colazione", "vado a lavoro", "pranzo",
+//                      "Studio", "Guardo la lezione del giorno", "ceno", 
+//                     "vado a dormire"]
+
+
+// for (let indice = 0; indice < azionidioggi.length; indice++) { 
+//     azionidioggi[indice] = (indice + 1) + " " + azionidioggi[indice];
+//     console.log(azionidioggi[indice]);
+// }
+
+
+
+// FINE SECONDO ESERCIZIO--------------------------------------------------------
+
+//TERZO ESERCIZIO 
+// let birthday = prompt ("inserisci il giorno del tuo compleanno","day/month/year")
+// if (birthday === "14/07/1990")
+// {console.log ("auguri, oggi è il tuo compleanno" ,'')}
+// else {console.log("oggi non è il tuo compleanno")}
+
+ // FINE TERZO ESERCIZIO -----------------------------
