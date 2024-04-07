a0e76676525a11d80cf2e66af19b17a1bf61a957
GabrieleLonghitano90 <gabrielelonghitano90@gmail.com>
2024-04-07 11:58:17 +0200

    Aggiornamento esercizi 05-04-2024

--- Esercizi 05-04-2024/main.js
+++ Esercizi 05-04-2024/main.js
@@ -1,8 +1,9 @@
-// PRIMO ESERCIZIO
+//PRIMO ESERCIZIO
 
-// let birthday = "12/07/1990" 
+// let birthday = "14/07/1990" 
+// let newdate = "14/07/1990"
 
-// if (birthday === "14/07/1990") 
+// if (birthday === newdate) 
 //  { console.log( '🎁' ) }
 // else { console.log("oggi non è il tuo compleanno") }
 
@@ -29,8 +30,34 @@
 
 //TERZO ESERCIZIO 
 // let birthday = prompt ("inserisci il giorno del tuo compleanno","day/month/year")
-// if (birthday === "14/07/1990")
-// {console.log ("auguri, oggi è il tuo compleanno" ,'')}
+// let newdate = "14/07/1990"
+// if (birthday === newdate)
+// {console.log ("auguri, oggi è il tuo compleanno" ,'🎁')}
 // else {console.log("oggi non è il tuo compleanno")}
 
  // FINE TERZO ESERCIZIO -----------------------------
+
+ //INIZIO QUARTO ESERCIZIO
+
+//  let primonumeroadd = 8
+//  let secondonumeroadd = 4
+//  let risultatoaddizione = primonumeroadd + secondonumeroadd
+//  console.log (risultatoaddizione)
+
+//  let primonumerosottr = 4
+//  let secondonumerosottr = 2
+//  let risultatosottrazione = primonumerosottr - secondonumerosottr
+//  console.log(risultatosottrazione)
+
+//  let primonumeroper = 4
+//  let secondonumeroper = 2
+//  let risultatomoltiplicazione = primonumeroper * secondonumeroper
+//  console.log(risultatomoltiplicazione)
+
+//  let primonumerodiv = 4
+//  let secondonumerodiv = 2
+//  let risultatodivisione = primonumerodiv / secondonumerodiv
+//  console.log(risultatodivisione)
+ 
+
+ //FINE QUARTO ESERCIZIO
---
+++ Esercizi random/main.js
@@ -0,0 +1,99 @@
+// PRIMO ESERCIZIO
+
+// let birthday = "12/07/1990" 
+
+// if (birthday === "14/07/1990") 
+//  { console.log( '🎁' ) }
+// else { console.log("oggi non è il tuo compleanno") }
+
+
+//----------------FINE PRIMO ESERCIZIO
+
+//-------------- INIZIO SECONDO ESERCIZIO
+
+
+// let azionidioggi = ["Mi sveglio","Mi alzo", "mi lavo la faccia",
+//                         "faccio colazione", "vado a lavoro", "pranzo",
+//                      "Studio", "Guardo la lezione del giorno", "ceno", 
+//                     "vado a dormire"]
+
+
+// for (let indice = 0; indice < azionidioggi.length; indice++) { 
+//     azionidioggi[indice] = (indice + 1) + " " + azionidioggi[indice];
+//     console.log(azionidioggi[indice]);
+// }
+
+
+
+// FINE SECONDO ESERCIZIO--------------------------------------------------------
+
+//TERZO ESERCIZIO 
+// let birthday = prompt ("inserisci il giorno del tuo compleanno","day/month/year")
+// if (birthday === "14/07/1990")
+// {console.log ("auguri, oggi è il tuo compleanno" ,'')}
+// else {console.log("oggi non è il tuo compleanno")}
+
+ // FINE TERZO ESERCIZIO -----------------------------
+
+//  let numero = 1;
+
+//  while (numero <= 5) {
+//      console.log(numero);
+//      numero++;
+//  }
+ 
+
+// const array = ['a', 'b', 'c', 'd', 'e'];
+// let indice = 0;
+
+
+// while (indice < array.length) {
+//     console.log(array[indice]);
+//     indice++; }
+
+    // let frutta = ["mela", "banana","pera","ananas","kiwi"];
+    // for (let indice=0 ; indice < frutta.length; indice++) {
+    // console.log (frutta[indice]) }
+
+    let primonumeroadd = 8
+    let secondonumeroadd = 4
+    // let primonumeropars = parseFloat(primonumeroadd)
+    // let secondonumeropars = parseFloat(secondonumeroadd)
+    let risultatoaddizione = (primonumeroadd + secondonumeroadd)
+    console.log (risultatoaddizione)
+
+    let primonumerosottr = 4
+    let secondonumerosottr = 2
+    let risultatosottrazione = primonumerosottr - secondonumerosottr
+    // console.log(risultatosottrazione)
+
+    let primonumeroper = 4
+    let secondonumeroper = 2
+    let risultatomoltiplicazione = primonumeroper * secondonumeroper
+    // console.log(risultatomoltiplicazione)
+
+    let primonumerodiv = 4
+    let secondonumerodiv = 2
+    let risultatodivisione = primonumerodiv / secondonumerodiv
+    // console.log(risultatodivisione)
+    
+
+    
+    
+    // console.log (risultatoaddizione,risultatosottrazione,risultatomoltiplicazione,risultatodivisione)
+
+// // Chiedi all'utente di inserire i due numeri separati da uno spazio tramite prompt
+// let numeriInseriti = prompt("Inserisci i due numeri separati da uno spazio:");
+
+// // Dividi la stringa inserita dall'utente in due numeri usando lo spazio come separatore
+// let numeriArray = numeriInseriti.split(" ");
+
+// // Converti entrambi i numeri in numeri in virgola mobile usando parseFloat
+// let primonumeroadd = parseFloat(numeriArray[0]);
+// let secondonumeroadd = parseFloat(numeriArray[1]);
+
+// // Esegui l'addizione dei due numeri
+// let risultatoaddizione = primonumeroadd + secondonumeroadd;
+
+// // Stampa il risultato
+// console.log("Il risultato dell'addizione è:", risultatoaddizione);
